cacc8151bc9005c55b216246e2d0ed7dbc7d036e
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-10-28 14:50:06 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import profile__img from "./assets/profile__img.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="profile__img__container" id="profile__img__container">
+        <img src={profile__img} alt="woman" id="profile__img" />
+        <h3 className="profile__name">Annette Black</h3>
+      </div>
+      <section className="profile">
+        <ul>
+          <li id="twitter">Twitter Link</li>
+          <li>Zuri Team</li>
+          <li>Zuri Books</li>
+          <li id="slack">Slack</li>
+          <li>Python Books</li>
+          <li>Background Check for Coders</li>
+          <li>Design Books</li>
+        </ul>
+      </section>
     </div>
   );
 }
