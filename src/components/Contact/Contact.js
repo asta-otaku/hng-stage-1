45f513e5dd5f52e7a760397db4a0322f70cfd4d2
asta-otaku <88886556+asta-otaku@users.noreply.github.com>
2022-11-04 12:27:10 +0100

    feat: Added validation to the form using yup and formik library

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Contact.css";
 import Footer from "../Footer/Footer";
+import * as Yup from "yup";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import TextError from "./TextError";
 
 function Contact() {
-  const [first_name, setFirst_Name] = useState("");
+  const initialValues = {
+    first_name: "",
+    last_name: "",
+    email: "",
+  };
+
+  const validationSchema = Yup.object({
+    first_name: Yup.string().required("Required!"),
+    last_name: Yup.string().required("Required!"),
+    email: Yup.string().email("Invalid Email Format!").required("Required"),
+  });
+
+  onsubmit = (values) => {
+    console.log("Form Data", values);
+  };
 
   return (
     <>
@@ -14,50 +31,72 @@ function Contact() {
           <span className="contact-span"> anything you have in mind.</span>
         </p>
         <div className="form-fields">
-          <form>
-            <div className="name-flex">
-              <div>
-                <label>First name</label>
-                <input
-                  type="text"
-                  id="first_name"
-                  placeholder="Enter your first name"
-                  onChange={(e) => setFirst_Name(e.target.value)}
-                />
-              </div>
-              <div>
-                <label>Last name</label>
-                <input
-                  type="text"
-                  id="last_name"
-                  placeholder="Enter your last name"
-                />
-              </div>
-            </div>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onsubmit}
+          >
+            {(formik) => (
+              <Form>
+                <div className="name-flex">
+                  <div>
+                    <label htmlFor="first_name">First name</label>
+                    <Field
+                      type="text"
+                      name="first_name"
+                      id="first_name"
+                      placeholder="Enter your first name"
+                    />
+                    <ErrorMessage name="first_name" component={TextError} />
+                  </div>
+                  <div>
+                    <label htmlFor="last_name">Last name</label>
+                    <Field
+                      name="last_name"
+                      type="text"
+                      id="last_name"
+                      placeholder="Enter your last name"
+                    />
+                    <ErrorMessage name="last_name" component={TextError} />
+                  </div>
+                </div>
 
-            <label>Email</label>
-            <input type="email" id="email" placeholder="yourname@email.com" />
-            <label>Message</label>
-            <textarea
-              id="message"
-              placeholder="Send me a message and I'll reply you as soon as possible"
-            />
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <input type="checkbox" />
-              <p>
-                You agree to providing your data to{" "}
-                <span className="span-check">
-                  {first_name ? first_name : "{name}"} who may contact you.
-                </span>
-              </p>
-            </div>
-            <button id="btn__submit">Send message</button>
-          </form>
+                <label htmlFor="email">Email</label>
+                <Field
+                  name="email"
+                  type="email"
+                  id="email"
+                  placeholder="yourname@email.com"
+                />
+                <ErrorMessage name="email" component={TextError} />
+                <label name="message">Message</label>
+                <textarea
+                  id="message"
+                  placeholder="Send me a message and I'll reply you as soon as possible"
+                />
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                  }}
+                >
+                  <input type="checkbox" />
+                  <p>
+                    You agree to providing your data to{" "}
+                    <span className="span-check">
+                      {formik.values.first_name
+                        ? formik.values.first_name
+                        : "{name}"}{" "}
+                      who may contact you.
+                    </span>
+                  </p>
+                </div>
+                <button type="submit" id="btn__submit">
+                  Send message
+                </button>
+              </Form>
+            )}
+          </Formik>
         </div>
       </div>
 
